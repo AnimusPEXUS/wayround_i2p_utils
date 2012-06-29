03e2f870c4e6551cf1d2494f27af279ddca0a199
Alexey V Gorshkov <animus@wayround.org>
2012-06-30 01:34:59 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='wayround_org_utils',
+      version='1.0',
+      description='Different service modules',
+      author='Alexey Gorshkov',
+      author_email='animus@wayround.org',
+      url='http://wiki.wayround.org/soft/utils',
+      packages=['wayround_org.utils'],
+     )
