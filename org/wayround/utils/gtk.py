35a13fca1a7367d256a5f853ee35ac8c85094283
Alexey V Gorshkov <animus@wayround.org>
2012-10-25 21:25:27 +0300

    IssueID #17
    Created new textview dialog box to acomplish #17.
    It contains special button to save text to file

--- org/wayround/utils/gtk.py
+++ org/wayround/utils/gtk.py
@@ -1,7 +1,9 @@
 
+import os.path
 import threading
-#import multiprocessing
 import logging
+
+import org.wayround.utils.gtk
 
 def widget_dict(builder):
 
@@ -75,3 +77,125 @@ class GtkSession:
             logging.debug("Joining thread")
             self._thr.join()
             logging.debug("Joined thread")
+
+class TextView:
+
+    def __init__(self):
+
+        from gi.repository import Gtk
+
+        ui_file = os.path.join(
+            os.path.dirname(__file__), 'ui', 'textview.glade'
+            )
+
+        ui = Gtk.Builder()
+        ui.add_from_file(ui_file)
+
+        self.ui = org.wayround.utils.gtk.widget_dict(ui)
+
+        self.ui['button1'].connect('clicked', self.onSaveAsActivated)
+
+    def onSaveAsActivated(self, button):
+
+        from gi.repository import Gtk
+
+        fc = Gtk.FileChooserDialog(
+            "Select File To Save List",
+            self.ui['window1'],
+            Gtk.FileChooserAction.SAVE,
+            (
+             Gtk.STOCK_SAVE, Gtk.ResponseType.OK,
+             Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL
+             )
+            )
+
+        rc_resp = fc.run()
+
+        path = fc.get_filename()
+
+        fc.destroy()
+
+        if rc_resp == Gtk.ResponseType.OK:
+
+            dialog_resp = Gtk.ResponseType.YES
+
+            if os.path.exists(path) and os.path.isdir(path):
+                dialog_resp = Gtk.ResponseType.NO
+
+                dia = Gtk.MessageDialog(
+                    self.ui['window1'],
+                    Gtk.DialogFlags.MODAL,
+                    Gtk.MessageType.ERROR,
+                    Gtk.ButtonsType.OK,
+                    "Directory not acceptable"
+                    )
+
+                dia.run()
+                dia.destroy()
+
+            elif os.path.exists(path) and os.path.isfile(path):
+
+                dialog_resp = Gtk.ResponseType.NO
+
+                dia = Gtk.MessageDialog(
+                    self.ui['window1'],
+                    Gtk.DialogFlags.MODAL,
+                    Gtk.MessageType.QUESTION,
+                    Gtk.ButtonsType.YES_NO,
+                    "File exists. Rewrite?"
+                    )
+
+                if dia.run() == Gtk.ResponseType.YES:
+                    dialog_resp = Gtk.ResponseType.YES
+
+                dia.destroy()
+
+            else:
+                pass
+
+            if (
+                (os.path.exists(path) and dialog_resp == Gtk.ResponseType.YES)
+                or
+                not os.path.exists(path)
+                ):
+
+                buff = self.ui['textview1'].get_buffer()
+                txt = buff.get_text(buff.get_start_iter(), buff.get_end_iter(), False)
+
+                try:
+                    f = open(path, 'w')
+                except:
+                    dia = Gtk.MessageDialog(
+                        self.ui['window1'],
+                        Gtk.DialogFlags.MODAL,
+                        Gtk.MessageType.ERROR,
+                        Gtk.ButtonsType.OK,
+                        "Couldn't rewrite file `{}'".format(path)
+                        )
+
+                    dia.run()
+                    dia.destroy()
+                else:
+
+                    f.write(txt)
+                    f.close()
+
+        return
+
+
+def text_view(text, title=''):
+
+    from gi.repository import Gtk
+
+    tw = TextView()
+
+    tb = Gtk.TextBuffer()
+    tb.set_text(str(text))
+
+    tw.ui['textview1'].set_buffer(tb)
+
+    tw.ui['window1'].set_title(str(title))
+
+    tw.ui['window1'].show_all()
+
+    return
