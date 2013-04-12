c5d17302e12e0ca199788bc241ab5959fb65de7b
Alexey V Gorshkov <animus@wayround.org>
2013-04-12 20:23:40 +0300

    moved prompt() back to "error" module, which was moved to "terminal" module by misstake

--- org/wayround/utils/error.py
+++ org/wayround/utils/error.py
@@ -1,5 +1,7 @@
 
+import sys
 import traceback
+import pprint
 
 def return_exception_info(e, tb=False):
     txt = """
@@ -28,3 +30,38 @@ def print_exception_info(e):
     print(txt)
     return
 
+def prompt(g=None, l=None, prompt='-> '):
+
+    import readline
+
+    bk = False
+
+    while not bk:
+
+        try:
+            cmd = input(prompt)
+            try:
+                e = eval(cmd, g, l)
+                if isinstance(e, dict):
+                    pprint.pprint(e, indent=2)
+                else:
+                    print(repr(e))
+            except SyntaxError:
+                exec(cmd, g, l)
+
+        except EOFError:
+            bk = True
+        except:
+            print(
+                return_exception_info(
+                    sys.exc_info(),
+                    tb=True
+                    )
+                )
+
+    print('')
+
+    return
+
+extract_stack = traceback.extract_stack
+format_list = traceback.format_list
