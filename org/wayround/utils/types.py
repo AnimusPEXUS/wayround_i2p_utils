739b1c847107021f7f2c7e1cd2f8ec061dec7d72
Alexey V Gorshkov <animus@wayround.org>
2013-10-03 03:37:16 +0300

    new structure checking functionality implemented

--- org/wayround/utils/types.py
+++ org/wayround/utils/types.py
@@ -182,3 +182,222 @@ def is{name}(obj):
 
 del i
 
+
+STRUCT_CHECK_KEYS = ['t', 'te', '!', '<', '>', '.', '', ' ', '{}']
+
+def struct_check(value, struct):
+
+    """
+    Check whatever value corresponds to struct
+
+    This implementation can't check dick with non-str keys, so exception is
+    raised in this case
+
+    Examples of `struct':
+
+
+    {
+    't': list, # type or tuple of types (like in isinstance).
+    'te': True, # exact type check with `type' function. default is True.
+    '!': False, # can value be None? (False - default)
+    '<': 0, # min child count. None - is default - no check
+    '>': None, # max child count. None - is default - no check
+    '.': { # check children. for work with sequences. default is None - not
+           #                                                 check children
+         't': list,
+         '<': 1,
+         '>': None,
+         '.': {
+              't': _Element
+              }
+         } # - default is None
+
+    # works only if 't' is str or bytes
+    '': False,
+    ' ': False,  # '' - string emptiness allow or not,
+                 # ' ' - only spaces in string allow or not
+
+    '{}': {}, # if 't' is dict. True - any keys are possible and
+              # each child checked with '.' value (recursion). False - same
+              # as True, but without values checking (dafault)
+              # if '{}' is dict, then all values in this dict must be dicts
+              # with structures like this.
+    }
+
+    return False if value does not matches struct, and True otherwise
+    """
+
+    ret = True
+
+    if not type(struct) == dict:
+        raise TypeError("`struct' must be dict")
+
+    for i in list(struct.keys()):
+        if not i in STRUCT_CHECK_KEYS:
+            raise ValueError("invalid `struct' key: {}".format(i))
+
+    typ = struct['t']
+
+    iterable_type = types(value)
+    iterable_type = (
+        'Sequence' in iterable_type
+        or 'Iterator' in iterable_type
+        or 'Iterable' in iterable_type
+        )
+
+
+    type_exact = True
+    if 'te' in struct:
+        type_exact = struct['te']
+        if type(type_exact) != bool:
+            raise TypeError("`te' must be bool")
+
+    can_be_none = False
+    if '!' in struct:
+        can_be_none = struct['!']
+        if type(can_be_none) != bool:
+            raise TypeError("`!' must be bool")
+
+    min_child_count = None
+    if '<' in struct:
+        min_child_count = struct['<']
+        if min_child_count != None and not type(min_child_count) == bool:
+            raise TypeError("`<' must be None or bool")
+
+    max_child_count = None
+    if '>' in struct:
+        max_child_count = struct['>']
+        if max_child_count != None and not type(max_child_count) == bool:
+            raise TypeError("`>' must be None or bool")
+
+    string_emptiness = True
+    if '' in struct:
+        string_emptiness = struct['']
+        if type(string_emptiness) != bool:
+            raise TypeError("`!' must be bool")
+
+    string_is_space = True
+    if ' ' in struct:
+        string_is_space = struct[' ']
+        if type(string_is_space) != bool:
+            raise TypeError("`!' must be bool")
+
+    next_test = None
+    if '.' in struct:
+        next_test = struct['.']
+        if next_test != None and not type(next_test) == dict:
+            raise TypeError("`.' must be None or dict")
+
+        if next_test != None and not iterable_type:
+            raise ValueError(
+                "`.' is not None and value is not a sequence or iterable"
+                )
+
+    dict_info = False
+    if '{}' in struct:
+        dict_info = struct['{}']
+        if not type(dict_info) in [bool, dict]:
+            raise TypeError(
+                "`{{}}' must be bool or dict, but it's a: {}".format(
+                    type(dict_info)
+                    )
+                )
+
+        keys = list(dict_info.keys())
+
+        for i in keys:
+            if not type(i) == str:
+                raise ValueError("keys in struct dict must be str")
+
+            if not type(dict_info[i]) == dict:
+                raise ValueError("values in struct dict must be dict")
+
+    if not can_be_none and value == None:
+        ret = False
+
+    if ret:
+        if type_exact == True:
+            if type(value) != typ:
+                ret = False
+        else:
+            if not isinstance(value, typ):
+                ret = False
+
+    if ret:
+        if min_child_count != None:
+            if len(value) < min_child_count:
+                ret = False
+
+    if ret:
+        if max_child_count != None:
+            if len(value) > max_child_count:
+                ret = False
+
+    if ret:
+        if type(value) == str and value == '' and string_emptiness == False:
+            ret = False
+
+    if ret:
+        if type(value) == bytes and value == b'' and string_emptiness == False:
+            ret = False
+
+    if ret:
+        if type(value) == str and value.isspace() and string_is_space == False:
+            ret = False
+
+    if ret:
+        if type(value) == bytes and value.isspace() and string_is_space == False:
+            ret = False
+
+    if ret:
+        if typ == dict:
+            if type(dict_info) == dict:
+
+                keys = list(dict_info.keys())
+                for i in keys:
+                    if not i in value:
+                        ret = False
+                        break
+                    else:
+                        ret = struct_check(value[i], dict_info[i])
+                        if ret == False:
+                            break
+
+                keys = list(value.keys())
+                for i in keys:
+                    if not i in dict_info:
+                        ret = False
+                        break
+
+            elif dict_info == False:
+                pass
+
+            elif dict_info == True:
+                vkeys = list(value.keys())
+                for i in vkeys:
+                    ret = struct_check(value[i], next_test)
+                    if ret == False:
+                        break
+            else:
+                raise Exception("Programming error")
+
+    if ret:
+        if next_test != None:
+            if iterable_type:
+                for i in value:
+                    if struct_check(i, next_test) == False:
+                        ret = False
+                        break
+
+            else:
+                ret = False
+
+    return ret
+
+
+
+
+
+
+
+
---
+++ tests/struct_check.py
@@ -0,0 +1,24 @@
+
+from org.wayround.utils.types import struct_check
+from lxml.etree import Element, _Element
+
+
+for i in [
+        ('', {'t': str}),
+        (1, {'t': str}),
+        (1, {'t': int}),
+        ([], {'t': list}),
+        ([1, 2, 3, 4], {'t': list, '.': {'t': str}}),
+        ([1, 2, 3, 4], {'t': list, '.': {'t': int}}),
+        (['1', '2', '3', '4'], {'t': list, '.': {'t': str}}),
+        (['1', 2, '3', '4'], {'t': list, '.': {'t': str}}),
+        ([Element('a'), Element('b')], {'t': list, '.': {'t': _Element}}),
+        ([dict('')], {'t': dict, '{}':{'name':{'t':list, '.': {'t':str}}, 'value': {'t': list, '.': {'t': int}}}}),
+        ({'name':['1', '2', '3', '4'], 'value': [1, 2, 3, 4]},
+         {'t': dict, '{}':{'name':{'t':list, '.': {'t':str}}, 'value': {'t': list, '.': {'t': int}}}}),
+        ({'name':['1', 2, '3', '4'], 'value': [1, 2, 3, 4]},
+         {'t': dict, '{}':{'name':{'t':list, '.': {'t':str}}, 'value': {'t': list, '.': {'t': int}}}}),
+]:
+    print(repr(i))
+    print(struct_check(i[0], i[1]))
+    print('')
