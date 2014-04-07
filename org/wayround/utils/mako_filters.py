dc73444c21225338057b98db68db6ff007bf8f7b
Alexey V Gorshkov <animus@wayround.org>
2014-04-07 07:34:44 +0400

    mako_filters.py deprecated

--- org/wayround/utils/mako_filters.py
+++ org/wayround/utils/mako_filters.py
@@ -1,13 +1,17 @@
 
+# TODO: figure out this
+raise Exception("Deprecated?")
+
 import urllib
 
 import mako.filters
-
-mako.filters.DEFAULT_ESCAPES['u_path'] = 'filters.u_path_escape'
 
 
 def u_path_escape(text):
     return urllib.parse.quote(text)
 
 
-mako.filters.u_path_escape = u_path_escape
+def install():
+
+    mako.filters.DEFAULT_ESCAPES['u_path'] = 'filters.u_path_escape'
+    mako.filters.u_path_escape = u_path_escape
