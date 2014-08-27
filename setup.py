d76541a0ad818b4881c7478d96e424709d2f00a2
Alexey V Gorshkov <animus@wayround.org>
2014-08-27 16:58:19 +0400

    ic

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ import os.path
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
 py_compil_args = None
 py_link_args = None
 
-p = subprocess.Popen(['pkg-config', '--cflags', 'python3'], stdout=subprocess.PIPE)
+p = subprocess.Popen(
+    ['pkg-config', '--cflags', 'python3'], stdout=subprocess.PIPE)
 p.wait()
 py_compile_args = str(p.communicate()[0], encoding='utf-8').split()
 
-p = subprocess.Popen(['pkg-config', '--libs', 'python3'], stdout=subprocess.PIPE)
+p = subprocess.Popen(
+    ['pkg-config', '--libs', 'python3'], stdout=subprocess.PIPE)
 p.wait()
 py_link_args = str(p.communicate()[0], encoding='utf-8').split()
 
 setup(
     name='org_wayround_utils',
-    version='0.14',
+    version='1.0',
     description='Various service modules',
     long_description="""\
 This package contains various useful modules functions and classes.
@@ -29,7 +30,7 @@ This package contains various useful modules functions and classes.
 
     author='Alexey Gorshkov',
     author_email='animus@wayround.org',
-    url='http://wiki.wayround.org/soft/org_wayround_utils',
+    url='https://github.com/AnimusPEXUS/org_wayround_utils',
     packages=[
         'org.wayround.utils',
         'org.wayround.utils.format'
@@ -37,7 +38,7 @@ This package contains various useful modules functions and classes.
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX',
-        'Development Status :: 1 - Planning'
+        'Development Status :: 5 - Production/Stable'
         ],
     ext_modules=[
         Extension(
@@ -46,18 +47,11 @@ This package contains various useful modules functions and classes.
             extra_compile_args=py_compile_args,
             extra_link_args=py_link_args,
             ),
-#        Extension(
-#            "org.wayround.utils.version",
-#            ["org/wayround/utils/version.pyx"],
-#            # TODO: pkg-config
-#            )
-        ],
-#    cmdclass={'build_ext': build_ext},
-    package_data={
-        'org.wayround.utils': [
-            'config.sub', 
-            os.path.join('format', '*.c'),
-            os.path.join('format', '*.h')
+        package_data={
+            'org.wayround.utils': [
+                'config.sub',
+                os.path.join('format', '*.c'),
+                os.path.join('format', '*.h')
             ]
         }
     )
