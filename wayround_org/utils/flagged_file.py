eff510a02dc92020a3f076dec4ecc5339b6c3e59
Alexey Gorshkov <animus@wayround.org>
2016-02-21 18:36:08 +0300

    permanent_variable module renamed into pm (permanent memory)

--- wayround_org/utils/flagged_file.py
+++ wayround_org/utils/flagged_file.py
@@ -187,11 +187,11 @@ class FlaggedFile:
 
         if not isinstance(
                 pv,
-                wayround_org.utils.permanent_memory.PermanentVariable
+                wayround_org.utils.pm.PermanentVariable
                 ):
             raise TypeError(
                 "must be inst of "
-                "wayround_org.utils.permanent_memory.PermanentVariable"
+                "wayround_org.utils.pm.PermanentVariable"
                 )
 
         ret = None
